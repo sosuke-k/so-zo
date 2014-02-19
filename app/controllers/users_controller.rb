9dcd01cd1f40406b3af758b7d57224a3bae30234
sosuke <snoopies.drum@gmail.com>
2014-02-19 01:35:53 -0800

    added header and footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,23 +10,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # POST /users
-  # POST /users.json
-  # def create
-  #   @user = User.new(user_params)
-
-  #   respond_to do |format|
-  #     if @user.save
-  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
-  #       # format.json { render action: 'show', status: :created, location: @user }
-  #     else
-  #       # format.html { redirect_to root_path, notice: 'User was unsccessfully created.' }
-  #       format.html { render action: 'new' }
-  #       # format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
   def create
     @user = User.new(user_params)
     if @user.save
